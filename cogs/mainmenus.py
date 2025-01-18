610a523be335d4b9735e2c4781e6e7c38395fe0d
AsherSSB <gromanenghi04@gmail.com>
2025-01-17 22:24:03 -0700

    response select class

--- cogs/mainmenus.py
+++ cogs/mainmenus.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import asyncio
 from custom.ui import ResponseView, ResponseButton
 
 
@@ -17,15 +16,21 @@ class Scheduler(commands.Cog):
             await view.interaction.response.defer()
             await interaction.delete_original_response()
         # TODO: add functionality to other choices
-        else:
-            await view.interaction.defer()
-            pass
 
 
 class ScheduleView(ResponseView):
     def __init__(self):
         super().__init__()
-        self.add_item(ResponseButton("Exit", -1, style=discord.ButtonStyle.red))
+        self.add_item(ResponseButton("View My Schedule", 0, style=discord.ButtonStyle.green, row=0))
+        self.add_item(ResponseButton("Set Availability", 1, style=discord.ButtonStyle.blurple, row=0))
+        self.add_item(ResponseButton("Set Schedule", 2, style=discord.ButtonStyle.blurple, row=1))
+        self.add_item(ResponseButton("Set Managers", 3, style=discord.ButtonStyle.red, row=3))
+        self.add_item(ResponseButton("Exit", -1, style=discord.ButtonStyle.red, row=4))
+
+
+class SetPotentialTimesView(ResponseView):
+    def __init__(self):
+        super().__init__()
 
 
 async def setup(bot):
--- custom/ui.py
+++ custom/ui.py
@@ -15,12 +15,27 @@ class ResponseView(ui.View):
 
 
 class ResponseButton(ui.Button):
-    def __init__(self, label, choice, style=discord.ButtonStyle.blurple, emoji=None):
-        super().__init__(label=label, style=style, emoji=emoji)
+    def __init__(self, label, choice, style=discord.ButtonStyle.blurple, emoji=None, row=None):
+        super().__init__(label=label, style=style, emoji=emoji, row=row)
         self.choice = choice
 
     async def callback(self, interaction):
         self.view.interaction = interaction
         self.view.choice = self.choice
+        self.view.event.set()
+
+
+class ResponseOption(ui.SelectOption):
+    def __init__(self, label: str, value: int, emoji=None):
+        super().__init__(label=label, value=value, emoji=emoji)
+
+
+class ResponseSelect(ui.Select):
+    def __init__(self, options: list[ResponseOption], row=None):
+        super().__init__(options=options, max_values=len(options), row=row)
+
+    async def callback(self, interaction):
+        self.view.interaction = interaction
+        self.view.choice = [int(value) for value in self.values]
         self.view.event.set()
 
