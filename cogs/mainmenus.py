cb9a7d4a41b011cb569b2c339e5a9aaa56ae6be7
AsherSSB <gromanenghi04@gmail.com>
2025-01-17 19:38:57 -0700

    hello world

---
+++ cogs/mainmenus.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+
+class Scheduler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.app_commands.command(name="schedule")
+    async def test_send_main_menu(self, interaction: discord.Interaction):
+        view = ScheduleView()
+        await interaction.response.send_message("Hello World!", view=view)
+        await view.wait()
+
+
+class ScheduleView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.choice: int
+        self.interaction: discord.Interaction
+        self.event = asyncio.Event()
+
+    @discord.ui.button(label="Back")
+    async def back_button(self, interaction: discord.Interaction, button):
+        self.choice = -1
+        self.event.set()
+        self.interaction = interaction
+
+    async def wait(self):
+        await self.event()
+
+
+async def setup(bot):
+    await bot.add_cog(Scheduler(bot))
+
+
