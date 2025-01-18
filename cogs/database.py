6604da90b35d46cb40b45e2b584919a081123872
AsherSSB <gromanenghi04@gmail.com>
2025-01-18 11:47:47 -0700

    adding database tables

--- cogs/database.py
+++ cogs/database.py
@@ -17,6 +17,23 @@ class Database(commands.Cog):
                                      port="5432")
         self.cur = self.conn.cursor()
         # TODO: initialize tables (team serial/userid primary keys)
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS scrimteams (
+                server_id BIGINT NOT NULL,
+                team_id SERIAL PRIMARY KEY,
+                team_name VARCHAR(100) NOT NULL,
+                players BIGINT[],
+                scrim_blocks VARCHAR[]
+            );
+
+            CREATE TABLE IF NOT EXISTS scrimplayers (
+                user_id BIGINT PRIMARY KEY,
+                team_id INTEGER REFERENCES scrimteams(team_id),
+                team_name VARCHAR(100) NOT NULL,
+                availability SMALLINT[]
+        );
+    """)
+        self.conn.commit()
 
 
 async def setup(bot):
